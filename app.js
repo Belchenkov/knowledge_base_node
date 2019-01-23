cf8f731c0fac7f5ed3fcdc82c8e0ddc709d42cbd
Алексей Бельченков <u608110@gmail.com>
2019-01-23 11:00:05 +0400

    Save Articles to MongoDB

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const app = express();
 
 // Bring in Models
 let Article = require('./models/Article');
 
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -30,6 +33,20 @@ app.get('/articles/add', (req, res) => {
     res.render('add_article', {
         title: 'Add Article'
     });
+});
+
+// Add Submit POST Route
+app.post('/articles/add', (req, res) => {
+   let article = new Article();
+   article.title = req.body.title;
+   article.author = req.body.author;
+   article.body = req.body.body;
+
+   article.save()
+       .then(result => {
+           res.redirect('/');
+       })
+       .catch(err => console.log(err));
 });
 
 const PORT = 5000;
